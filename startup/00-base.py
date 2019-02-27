182bcce9aea6b761fc96b00a7144f5f704b2dc6c
Maksim Rakitin <mrakitin@bnl.gov>
2019-02-27 13:24:26 -0500

    Move bp* stuff to 90-plans

--- startup/00-base.py
+++ startup/00-base.py
@@ -1,6 +1,5 @@
 from bluesky.run_engine import RunEngine
 from databroker import Broker, temp_config
-import bluesky.plans as bp
 
 
 RE = RunEngine({})
---
+++ startup/90-plans.py
@@ -0,0 +1,5 @@
+import bluesky.plans as bp
+import bluesky.plan_stubs as bps
+import bluesky.preprocessors as bpp
+
+
